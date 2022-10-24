79c680590932d51bd5e007666698d6f90b5662da
hydok <hydok@naver.com>
2022-10-24 17:32:09 +0900

    usecase

--- domain/src/main/java/io/hydok/domain/usecase/GetSampleDataUseCase.kt
+++ domain/src/main/java/io/hydok/domain/usecase/GetSampleDataUseCase.kt
@@ -3,4 +3,7 @@ package io.hydok.domain.usecase
 import io.hydok.domain.repository.Repository
 
 class GetSampleDataUseCase(val repository: Repository) {
+    operator fun invoke(query: String) {
+
+    }
 }
