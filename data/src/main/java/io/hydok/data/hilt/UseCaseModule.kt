3ea537a086627938757f595b8b2594d99762178f
hydok <hydok@naver.com>
2024-05-07 14:12:29 +0900

    클래스 이름 수정

--- data/src/main/java/io/hydok/data/hilt/UseCaseModule.kt
+++ data/src/main/java/io/hydok/data/hilt/UseCaseModule.kt
@@ -4,12 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import io.hydok.data.usecaseImpl.UseCaseImpl
+import io.hydok.data.usecaseImpl.GetSampleDataUseCaseImpl
 import io.hydok.domain.usecase.GetSampleDataUseCase
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class UseCaseModule {
     @Binds
-    abstract fun bindGetSampleDataUseCase(impl: UseCaseImpl): GetSampleDataUseCase
+    abstract fun bindGetSampleDataUseCase(impl: GetSampleDataUseCaseImpl): GetSampleDataUseCase
 }
