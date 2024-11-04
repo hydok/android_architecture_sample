9b1109a1eeab8a715f428b60c134bf1e0066287a
hecto <hydok@naver.com>
2024-11-04 16:35:33 +0900

    test

--- data/src/main/java/io/hydok/data/mapper/Mapper.kt
+++ data/src/main/java/io/hydok/data/mapper/Mapper.kt
@@ -8,6 +8,7 @@ import io.hydok.domain.model.SampleModel
  * Data Layer에서 타 계층으로 Data전달 시 해당 Layer에 사용되도록 변환
  */
 fun mapperToUserData(entitys: List<SampleEntity>): List<SampleModel> {
+    //아하하하
     return entitys.map {
         SampleModel(
             it.id.toInt(),
